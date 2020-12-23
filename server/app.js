06081f10ccce90d8c4b36ef037045faa7ab0669c
vortex2099 <viveksinghrana2345@gmail.com>
2020-12-24 01:26:59 +0530

    start files

---
+++ server/app.js
@@ -0,0 +1,22 @@
+//sxl5LTvgO2hOjOTO
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const PORT = 5000
+const { MONGOURI } = require('./keys')
+
+
+mongoose.connect(MONGOURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+mongoose.connection.on('connected', () => {
+    console.log("connected to mongoose")
+})
+mongoose.connection.on('error', (err) => {
+    console.log("connecting error", err)
+})
+
+app.listen(PORT, () => {
+    console.log("server is running on", PORT)
+})
---
+++ server/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGOURI: "mongodb+srv://vivek:sxl5LTvgO2hOjOTO@cluster0.7zm3d.mongodb.net/<dbname>?retryWrites=true&w=majority"
+}  
