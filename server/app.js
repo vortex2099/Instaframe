9f128213675a83057fff6e225329de00c5772d56
vortex2099 <viveksinghrana2345@gmail.com>
2020-12-24 18:24:04 +0530

    added post route

--- server/app.js
+++ server/app.js
@@ -6,9 +6,7 @@ const PORT = 5000
 const { MONGOURI } = require('./keys')
 
 
-require('./models/user')
-app.use(express.json())
-app.use(require('./routes/auth'))
+
 mongoose.connect(MONGOURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -19,6 +17,11 @@ mongoose.connection.on('connected', () => {
 mongoose.connection.on('error', (err) => {
     console.log("connecting error", err)
 })
+
+require('./models/user')
+require('./models/post')
+app.use(express.json())
+app.use(require('./routes/auth'))
 
 app.listen(PORT, () => {
     console.log("server is running on", PORT)
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,9 +8,7 @@ const { JWT_SECRET } = require('../keys')
 const requireLogin = require('../middleware/requireLogin')
 
 
-router.get('/protected', requireLogin, (req, res) => {
-    res.send("hello")
-})
+
 
 
 
