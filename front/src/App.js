be4479f0e2df7af17ee170cd094cbfdb7c21d568
vortex2099 <viveksinghrana2345@gmail.com>
2020-12-26 02:33:43 +0530

    started frontend

---
+++ front/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css';
+import NavBar from './components/Navbar'
+
+function App() {
+  return (
+    <NavBar />
+  );
+}
+
+export default App;
---
+++ front/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div class="nav-wrapper">
+                <a href="#" class="brand-logo left">Instaframe</a>
+                <ul id="nav-mobile" class="right ">
+                    <li><a href="sass.html">Sass</a></li>
+                    <li><a href="badges.html">Components</a></li>
+                    <li><a href="collapsible.html">JavaScript</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+export default Navbar
