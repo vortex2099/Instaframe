1b59ce588fb8988c074f3b668aa6668ca8beba4a
vortex2099 <viveksinghrana2345@gmail.com>
2020-12-26 10:42:01 +0530

    added Home Profile Signin and Signup pages

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,35 @@
 import React from 'react'
 import './App.css';
 import NavBar from './components/Navbar'
+import Signin from './components/screens/SignIn'
+import Signup from './components/screens/SignUp'
+import Home from './components/screens/Home'
+import Profile from './components/screens/Profile'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+
 
 function App() {
-  return (
+  return (<BrowserRouter>
+
     <NavBar />
+    <Route exact path='/'>
+      <Home />
+    </Route>
+    <Route path='/signin'>
+      <Signin />
+    </Route>
+    <Route path='/signup'>
+      <Signup />
+    </Route>
+    <Route path='/profile'>
+      <Profile />
+    </Route>
+
+  </BrowserRouter>
+
+
   );
 }
 
-export default App;
+export default App; 
--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <nav>
             <div class="nav-wrapper">
-                <a href="#" class="brand-logo left">Instaframe</a>
-                <ul id="nav-mobile" class="right ">
-                    <li><a href="sass.html">Sass</a></li>
-                    <li><a href="badges.html">Components</a></li>
-                    <li><a href="collapsible.html">JavaScript</a></li>
+                <Link Link to="/" className="brand-logo left">Instaframe</Link>
+                <ul id="nav-mobile" className="right ">
+                    <li><Link Link to="/signin">Signin</Link></li>
+                    <li><Link Link to="signup">Signup</Link></li>
+                    <li><Link Link to="/profile">Profile</Link></li>
                 </ul>
             </div>
         </nav>
